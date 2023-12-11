b3ce816077bc8211cf3a83ed2114a1aede1e4bf0
ludiaz978 <142175671+ludiaz978@users.noreply.github.com>
2023-12-10 19:01:57 -0800

    day 1 complete

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
-// require express
+// require express and CORS and jsonwebtoken
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+//require and configure dotenv
+require('dotenv').config();
+
+//load and store our JWT secret key
+const secretKey = process.env.JWT_SECRET;
+
 //create the express app
 const app = express();
- //setup MIDDLEWARE
-
-
-// CORS middleware
+ 
+      //setup MIDDLEWARE
+  // CORS middleware
 app.use(cors());
 
 // logging midleware
@@ -17,12 +24,55 @@ app.use((req, res, next) => {
 next();
 })
 
-// JSON parsing middleware
+  // JSON parsing middleware
 app.use(express.json());
 
-//create local storage array for todos
+  //create local storage array for todos
 let todos = [{id: 1, task: "wash dishes"}];
 
+  //create local storage array for users
+let users = [
+ {id: 1, username: "luradiaz", password: "password1"},
+ {id: 2, username: "johndoe", password: "password2"}
+];
+
+//HTTP Handlers related to login
+// mock get request for login page
+app.get('/login', (req, res) => {
+ res.send("Login Page");  
+})
+
+//create post handler to handle authentication
+app.post('/login', (req, res) => {
+   // extract usename and password from the request body
+   const {username, password } = req.body; // assumes body will contain username and password 
+
+  //find a user in users array whose credentials match those sent in the request
+  //array.find() => iterates through the array and runs each item through a function
+  //that returns a boolean. If the function returns true, then find stops and returns
+  // the item it was on
+  const user = users.find(u => u.username === username && u.password === password);
+
+//check if a matching user was found
+if(user) {
+    //user is found, so create a JWT token
+    //jwt.sign creates a new JWT with the specified payload and secret key
+const token = jwt.sign(
+    {userId: user.id}, // payload: contains user info, eg. user id
+    secretKey, //the secret key used for signing the token
+    { expiresIn: '1h'}//sets token expiration time to 1 hour
+    );
+
+    //send the created token back in the response
+    res.json({token});
+} else{
+    //no user was found so send back 401 unauthorized response
+    res.status(401).send("Authentication Failed");
+}
+
+}).
+
+//http Handlers related to todo data
 // create a get handler to the path/todos that sends back the todos
 app.get('/todos', (req, res) => {
 //sends back todos in JSON format
